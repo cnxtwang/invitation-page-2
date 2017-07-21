569730587edb045d2a3e73625152039c70ff2d0e
xtwang <xtwang@thoughtworks.com>
2017-07-21 16:17:08 +0800

    add event listener to handle the click and input

--- src/js/invitation.js
+++ src/js/invitation.js
@@ -1,2 +1,39 @@
 import '../style/invitation.scss'
 import '../../index.html'
+
+let $availableNumber = document.getElementById('availableNumber')
+let $users = document.getElementsByName('user')
+let $eMail = document.getElementById('eMail')
+let $invite = document.getElementById('invite')
+let $main = document.getElementsByTagName('main')[0]
+
+const calcAvailableNumber = () => {
+  let $selectedUsers = document.querySelectorAll('input[name="user"]:checked')
+  $availableNumber.innerText = $users.length - $selectedUsers.length
+
+  let isDisabledInvite = !($selectedUsers.length || $eMail.value);
+  $invite.disabled = isDisabledInvite
+  $invite.className = isDisabledInvite ? 'disabled' : ''
+}
+
+$main.addEventListener('click', evt => {
+  if(evt.target.type !== 'checkbox') return
+  calcAvailableNumber()
+})
+
+$eMail.addEventListener('keyup', () => {
+  calcAvailableNumber()
+})
+
+$invite.addEventListener('click', () => {
+  let message = ''
+
+  let $selectedUsers = document.querySelectorAll('input[name="user"]:checked')
+  $selectedUsers.forEach(user => {
+    message += user.value + ' '
+  })
+
+  alert(message + $eMail.value + ' has been invited')
+})
+
+calcAvailableNumber()
